ae1f0602cc796caf39e5286c59c646b0b5b87948
keroMaged0 <iti.202020138@thebes.edu.eg>
2025-03-06 00:19:50 +0200

    feat(order): improve order handling and validation

--- src/controllers/orders/add-order.controller.ts
+++ src/controllers/orders/add-order.controller.ts
@@ -6,9 +6,11 @@ import {
   PaymentMethod,
 } from "../../models/order.model";
 import { SuccessResponse } from "../../types/responses.type";
+import { ErrorCodes } from "../../types/errors-code.type";
 import { Product } from "../../models/product.model";
 import { Cart } from "../../models/cart.model";
 import { logger } from "../../config/winston";
+import { Errors } from "../../errors";
 
 interface OrderResponseBody {
   shipping_address: IShippingAddress;
@@ -25,39 +27,29 @@ export const addOrderHandler: RequestHandler<
 
   try {
     const cart = await Cart.findOne({ user_id: userId });
-    if (!cart?.items.length) {
-      res.status(400).json({
-        success: false,
-        message: "Cart is empty",
-        data: {},
-      });
-      return;
+    if (!cart || cart.items.length === 0) {
+      return next(new Errors.BadRequest(ErrorCodes.CART_EMPTY));
     }
 
-    let totalOrderPrice = cart.total_price_after_discount
-      ? cart.total_price_after_discount
-      : cart.sub_total;
+    let totalOrderPrice = cart.total_price_after_discount || cart.sub_total;
 
-    const order = new Order({
+    const order = await new Order({
       order_items: cart.items,
       user_id: userId,
       shipping_address,
       payment_method,
       sub_total: cart.sub_total,
       total_price_after_discount: totalOrderPrice,
-    });
+    }).save();
 
-    if (order) {
-      let options = cart.items.map((item) => ({
-        updateOne: {
-          filter: { _id: item.product },
-          update: { $inc: { stock: -item.quantity } },
-        },
-      }));
+    let bulkUpdates = cart.items.map((item) => ({
+      updateOne: {
+        filter: { _id: item.product },
+        update: { $inc: { stock: -item.quantity } },
+      },
+    }));
 
-      await Product.bulkWrite(options);
-      await order.save();
-    }
+    await Product.bulkWrite(bulkUpdates);
 
     await Cart.findByIdAndDelete(cart._id);
 
