98ca552dfe466bb4749ef59e6bda0410389023dc
keroMaged0 <iti.202020138@thebes.edu.eg>
2025-03-08 01:02:03 +0200

    Fix: Use /tmp directly for logging in Vercel

--- src/config/winston.ts
+++ src/config/winston.ts
@@ -1,13 +1,8 @@
 import DailyRotateFile from "winston-daily-rotate-file";
 import winston from "winston";
 import path from "path";
-import fs from "fs";
 
-const logDirectory = "/tmp/logs";
-
-if (!fs.existsSync(logDirectory)) {
-  fs.mkdirSync(logDirectory, { recursive: true });
-}
+const logDirectory = "/tmp";
 
 export const logger = winston.createLogger({
   format: winston.format.combine(
@@ -16,7 +11,9 @@ export const logger = winston.createLogger({
     winston.format.simple(),
     winston.format.printf((info: any) => {
       if (info.stack) {
-        return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}\n${info.stack}`;
+        return `${info.timestamp} [${info.level.toUpperCase()}]: ${
+          info.message
+        }\n${info.stack}`;
       }
       return `${info.timestamp} [${info.level.toUpperCase()}]: ${info.message}`;
     })
@@ -26,7 +23,7 @@ export const logger = winston.createLogger({
     new winston.transports.Console(),
 
     new DailyRotateFile({
-      filename: path.join(logDirectory, "logfile-%DATE%.log"), 
+      filename: path.join(logDirectory, "logfile-%DATE%.log"),
       datePattern: "YYYY-MM-DD",
       zippedArchive: false,
       maxSize: "20m",
