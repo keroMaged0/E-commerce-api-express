13ebecbad0361971f3d913bab49309035a3d1269
keroMaged0 <iti.202020138@thebes.edu.eg>
2025-03-08 21:47:05 +0200

    Update stripe webhook

--- src/controllers/payment/stripe-webhook.controller.ts
+++ src/controllers/payment/stripe-webhook.controller.ts
@@ -8,49 +8,48 @@ import { env } from "../../config/env";
 
 export const stripeWebhookHandler = async (req: Request, res: Response) => {
   const sig = req.headers["stripe-signature"];
-  const webhookSecret = env.payment.stripe.stripeWebhookSecret;
-
   let event;
+
   try {
     event = stripe.webhooks.constructEvent(
       req.body,
       sig as string,
-      webhookSecret
+      env.payment.stripe.stripeWebhookSecret
     );
   } catch (error: any) {
     logger.error("Webhook Error: " + error.message);
     res.status(400).send("Webhook Error: " + error.message);
-    return;
   }
 
-  switch (event.type) {
-    case "checkout.session.completed":
-      const session = event.data.object as any;
-      const paymentId = session.client_reference_id;
-      const orderId = session.metadata?.orderId;
-      try {
-        await Payment.findByIdAndUpdate(paymentId, {
-          payment_status: PaymentStatus.COMPLETED,
-        });
-        if (orderId) {
-          await Order.findByIdAndUpdate(orderId, { is_paid: true });
-        }
-        logger.info(`Payment ${paymentId} succeeded.`);
-      } catch (error: any) {
-        logger.error(`Error updating payment ${paymentId}: ${error.message}`);
-        res.status(500).send("Internal Server Error");
-        return;
-      }
-      break;
-
-    default:
-      logger.info(`Unhandled event type ${event.type}`);
+  if (event.type === "checkout.session.completed") {
+    logger.info("Checkout session completed successfully");
+  } else {
+    logger.info(`Unhandled event type ${event.type}`);
   }
 
-  console.log("====================================");
-  console.log(sig);
-  console.log("====================================");
-  console.log(event);
+  // switch (event.type) {
+  //   case "checkout.session.completed":
+  //     const session = event.data.object as any;
+  //     const paymentId = session.client_reference_id;
+  //     const orderId = session.metadata?.orderId;
+  //     try {
+  //       await Payment.findByIdAndUpdate(paymentId, {
+  //         payment_status: PaymentStatus.COMPLETED,
+  //       });
+  //       if (orderId) {
+  //         await Order.findByIdAndUpdate(orderId, { is_paid: true });
+  //       }
+  //       logger.info(`Payment ${paymentId} succeeded.`);
+  //     } catch (error: any) {
+  //       logger.error(`Error updating payment ${paymentId}: ${error.message}`);
+  //       res.status(500).send("Internal Server Error");
+  //       return;
+  //     }
+  //     break;
+
+  //   default:
+  //     logger.info(`Unhandled event type ${event.type}`);
+  // }
 
   res.status(200).json({ received: true });
 };
