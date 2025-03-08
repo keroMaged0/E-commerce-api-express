70b29423b89986042fab9bd71e0fee0fd526ec75
keroMaged0 <iti.202020138@thebes.edu.eg>
2025-03-08 15:02:13 +0200

    Update success url in payment

--- src/services/payment.service.ts
+++ src/services/payment.service.ts
@@ -27,7 +27,7 @@ export class PaymentGateway {
           },
         ],
         mode: "payment",
-        success_url: `${env.frontUrl}/success?session_id={{CHECKOUT_SESSION_ID}}`,
+        success_url: `${env.frontUrl}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${env.frontUrl}/cancel`,
         client_reference_id: params.paymentId,
         metadata: {
