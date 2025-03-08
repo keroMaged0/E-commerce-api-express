0c3195b6317761972bf2fe098d50179837ae7d96
keroMaged0 <iti.202020138@thebes.edu.eg>
2025-03-08 20:49:50 +0200

    Add success url in app

--- src/app.ts
+++ src/app.ts
@@ -19,6 +19,21 @@ app.get("/", (req, res) => {
   res.send("API is working!");
 });
 
+app.get("/success", (req, res) => {
+  const sessionId = req.query.session_id;
+  if (!sessionId) {
+    res.status(400).json({ message: "session_id is required" });
+  }
+  res.json({
+    success: true,
+    message: `Payment Successful! Session ID: ${sessionId}`,
+  });
+});
+
+app.get("/cancel", (req, res) => {
+  res.send("cancel");
+});
+
 app.use("/api/v1", appRoutes);
 
 app.use("*", Middlewares.routeNotFound);
