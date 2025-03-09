2b32853d0b1f2f01e8cbe670c60d47398d97cc72
keroMaged0 <iti.202020138@thebes.edu.eg>
2025-03-10 01:51:11 +0200

    Add paginate and search

--- src/controllers/products/get-products.controller.ts
+++ src/controllers/products/get-products.controller.ts
@@ -2,6 +2,7 @@ import { RequestHandler } from "express";
 
 import { SuccessResponse } from "../../types/responses.type";
 import { Product } from "../../models/product.model";
+import { ApiFeature } from "../../utils/api-feature";
 import { logger } from "../../config/winston";
 
 export const getProductsHandler: RequestHandler<
@@ -10,7 +11,11 @@ export const getProductsHandler: RequestHandler<
   unknown
 > = async (req, res, next) => {
   try {
-    const products = await Product.find().populate([
+    const apiFeature = new ApiFeature(Product.find(), req.query)
+      .paginate()
+      .search();
+
+    const products = await apiFeature.query.populate([
       { path: "brand_id", select: "name" },
       { path: "category_id", select: "name" },
       { path: "reviews", select: "-_id user rating comment" },
